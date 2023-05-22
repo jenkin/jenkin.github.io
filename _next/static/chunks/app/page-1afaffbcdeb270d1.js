024cc76baaf3820ee15ee8f52f69db56e186663e
Alessio Cimarelli <jenkin@dataninja.it>
2023-05-22 11:38:22 +0200

    Build website

---
+++ _next/static/chunks/app/page-1afaffbcdeb270d1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{1515:function(n,e,_){Promise.resolve().then(_.t.bind(_,8219,23))},8219:function(n){n.exports={main:"page_main__FWdGo"}}},function(n){n.O(0,[38,34,744],function(){return n(n.s=1515)}),_N_E=n.O()}]);
