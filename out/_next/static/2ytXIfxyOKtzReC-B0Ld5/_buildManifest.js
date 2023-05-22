b7e17e3c3f1a435ddaf5fb2e816a88ce27318e5c
Alessio Cimarelli <jenkin@dataninja.it>
2023-05-22 11:38:22 +0200

    Build website

---
+++ out/_next/static/2ytXIfxyOKtzReC-B0Ld5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b91753ef1eb350c0.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
